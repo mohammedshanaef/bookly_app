2de97769de4e19f5eed54c3e0246f938d79bde69
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-01-06 19:05:15 +0300

    create home repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bookly_app/constant.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
